466e95ddd83f4514497b0b8b97185080174deed0
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-03 16:34:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL','filmztube')
-auth_grp = environ.get('AUTH_GROUP','filmz_tube')
+auth_channel = environ.get('AUTH_CHANNEL','-1001863340529')
+auth_grp = environ.get('AUTH_GROUP','-1001914868571')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
