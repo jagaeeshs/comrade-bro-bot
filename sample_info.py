eb660153984b29da93ed1a35cb20bfc6b7454e0e
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-03 15:43:36 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 4234473
+API_HASH = '6d9e82c9edc244b0c0a20d8fa89f9784'
+BOT_TOKEN = '6268719215:AAFXrTuEhXw6j8vP-40yQC35ixD_NnUGqQI'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
-AUTH_USERS = []
+ADMINS = ['bharath_boy2',634637418 ]
+CHANNELS = [ 'filmztube']
+AUTH_USERS = [634637418]
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
-COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb://karthik:karthik@ac-sry7xij-shard-00-00.wlp6rd4.mongodb.net:27017,ac-sry7xij-shard-00-01.wlp6rd4.mongodb.net:27017,ac-sry7xij-shard-00-02.wlp6rd4.mongodb.net:27017/?ssl=true&replicaSet=atlas-efnevi-shard-0&authSource=admin&retryWrites=true&w=majority"
+DATABASE_NAME = 'bharath'
+COLLECTION_NAME = 'Anurag_files'  # If you are using the same database, then use different collection name for each bot
 
 
