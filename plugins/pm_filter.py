14db988b871eb0192dbd43a7b0b8ed269a03dd07
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-04 06:36:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -45,7 +45,7 @@ async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
-        await cb_handler(client, msg, message)
+        await cb_handler(client, message)
 	    
 
 @Client.on_callback_query(filters.regex('rename'))
