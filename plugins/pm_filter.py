d4f9835a1e48ee4cced99ac0739be0aee338f64c
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-04 07:22:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -11,7 +11,7 @@ import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from info import *
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ForceReply, message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ForceReply
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
