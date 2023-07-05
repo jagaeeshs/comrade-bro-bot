fdf6c3365711526a1aaa83c8a38cd3423c8d03e8
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-05 07:36:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -545,7 +545,7 @@ async def cb_handler(client: Client , query: CallbackQuery):
                 f_caption += "\n\n•This file will be automatically deleted after 24 hours\n•Please save it to saved message or forward it anywhere."
             except Exception as e:
                 logger.exception(e)
-            
+            f_caption = f_caption
         if f_caption is None:
             f_caption = f"{files.file_name}"
             f_caption += "\n\n•This file will be automatically deleted after 24 hours\n•Please save it to saved message or forward it anywhere."
@@ -591,9 +591,11 @@ async def cb_handler(client: Client , query: CallbackQuery):
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
                                                        file_caption='' if f_caption is None else f_caption)
+                
+            f_caption += "\n\n•This file will be automatically deleted after 24 hours\n•Please save it to saved message or forward it anywhere."
             except Exception as e:
                 logger.exception(e)
-                #f_caption = f_caption
+                f_caption = f_caption
         if f_caption is None:
             f_caption = f"{title}"
         await query.answer()
