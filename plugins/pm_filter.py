d16775b89e12cc84b9be92b38b39227b481fe94e
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-05 06:14:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -533,7 +533,7 @@ async def cb_handler(client: Client , query: CallbackQuery):
         settings = await get_settings(query.message.chat.id)
     elif query.data.startswith('forward_'):
         #file_id = query.data.split('_')[1]
-        message_id = query.message.message_id
+        message_id = query.message.id
         await client.forward_messages(chat_id=query.message.chat.id, from_chat_id=query.from_user.id, message_ids=message_id)
 
         if CUSTOM_FILE_CAPTION:
