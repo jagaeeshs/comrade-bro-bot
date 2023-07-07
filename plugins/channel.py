c4fbb6084d93d515696279854404d627889ddae2
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-07 14:09:57 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -45,7 +45,6 @@ async def start(client, message):
     except Exception as e:
         await message.reply_text(f"Error: {str(e)}")
 
-
 @Client.on_message(filters.command("sendall") & filters.user(ADMINS))
 async def x(app, msg):
     args = msg.text.split(maxsplit=1)
@@ -56,7 +55,6 @@ async def x(app, msg):
         args = int(args)
     except Exception:
         return await msg.reply_text("Chat Id must be an integer not a string")
-
     jj = await msg.reply_text("Processing...")
     documents = col.find({})
     last_msg = col.find_one({'_id': 'last_msg'})
@@ -65,85 +63,7 @@ async def x(app, msg):
         last_msg = 0
     else:
         last_msg = last_msg.get('index', 0)
-    id_list = [{'id': document['_id'], 'file_name': document.get('file_name', 'N/A'), 'file_caption': document.get('caption', 'N/A'), 'file_size': document.get('file_size', 'N/A')} for document in documents]
-    await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}")
-
-    # Batch processing variables
-    batch_size = 100
-    num_batches = (len(id_list) + batch_size - 1) // batch_size
-
-    last_msg_batch = last_msg // batch_size
-    last_msg_index = last_msg % batch_size
-
-
-    # Process files in batches starting from the last message index
-    for batch in range(last_msg_batch, num_batches):
-        start_index = batch * batch_size
-        end_index = min((batch + 1) * batch_size, len(id_list))
-        batch_files = id_list[start_index:end_index]
-
-        batch_num = batch + 1
-        current_batch_files = len(batch_files)
-        total_files = len(id_list)
-        #await jj.edit(f"Found {total_files} Files In The DB Starting To Send In Chat {args}\nProcessing Batch {batch_num}/{num_batches}\nCurrent Batch Files: {current_batch_files}")
-
-        for j, i in enumerate(batch_files, start=last_msg_index if batch == last_msg_batch else 0):
-            try:
-                try:
-                    await app.send_video(
-                        msg.chat.id,
-                        i['id'],
-                        caption=CUSTOM_FILE_CAPTION.format(
-                            file_name=i['file_name'],
-                            file_caption=i['file_caption'],
-                            file_size=get_size(int(i['file_size']))
-                        )
-                    )
-                except Exception as e:
-                    print(e)
-                    await app.send_document(
-                        msg.chat.id,
-                        i['id'],
-                        caption=CUSTOM_FILE_CAPTION.format(
-                            file_name=i['file_name'],
-                            file_caption=i['file_caption'],
-                            file_size=get_size(int(i['file_size']))
-                        )
-                    )
-                await jj.edit(f"Found {total_files} Files In The DB Starting To Send In Chat {args}\nProcessing Batch {batch_num}/{num_batches}\nCurrent Batch Files: {current_batch_files}\nProcessed Files: {j+1}/{current_batch_files}")
-                col.update_one({'_id': 'last_msg'}, {'$set': {'index': start_index + j + 1}}, upsert=True)  # Update last_msg index within the batch
-                await asyncio.sleep(random.randint(2, 5))
-            except FloodWait as e:
-                print(f"Sleeping for {e.value} seconds.")
-                await asyncio.sleep(e.value)
-            except Exception as e:
-                print(e)
-
-        
-
-    await jj.delete()
-    await msg.reply_text("Completed")
-
-
-'''@Client.on_message(filters.command("sendall") & filters.user(ADMINS))
-async def x(app, msg):
-    args = msg.text.split(maxsplit=1)
-    if len(args) == 1:
-        return await msg.reply_text("Give Chat ID Also Where To Send Files")
-    args = args[1]
-    try:
-        args = int(args)
-    except Exception:
-        return await msg.reply_text("Chat Id must be an integer not a string")
-    jj = await msg.reply_text("Processing...")
-    documents = col.find({})
-    last_msg = col.find_one({'_id': 'last_msg'})
-    if not last_msg:
-        col.update_one({'_id': 'last_msg'}, {'$set': {'index': 0}}, upsert=True)
-        last_msg = 0
-    else:
-        last_msg = last_msg.get('index', 0)
-    id_list = [{'id': document['_id'], 'file_name': document.get('file_name', 'N/A'), 'file_caption': document.get('caption', 'N/A'), 'file_size': document.get('file_size', 'N/A')} for document in documents]
+    id_list = [{'id': document['_id'], 'file_name': document.get('file_name', 'N/A'), 'file_caption': document.get('caption', 'N/A'), 'file_size': document.get('file_size', 0)} for document in documents]
     await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}")
     for j, i in enumerate(id_list[last_msg:], start=last_msg):
         try:
@@ -154,14 +74,14 @@ async def x(app, msg):
                 await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=get_size(int(i['file_size']))))
             await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed: {j+1}")
             col.update_one({'_id': 'last_msg'}, {'$set': {'index': j}}, upsert=True)
-            await asyncio.sleep(random.randint(1, 3))
+            await asyncio.sleep(random.randint(2, 5))
         except FloodWait as e:
-            print(f"Sleeping for {e.x} seconds.")
-            await asyncio.sleep(e.x)
+            print(f"Sleeping for {e.value} seconds.")
+            await asyncio.sleep(e.value)
         except Exception as e:
             print(e)
     await jj.delete()
-    await msg.reply_text("Completed")'''
+    await msg.reply_text("Completed")
 
 @Client.on_message(filters.command("sendkey") & filters.user(ADMINS))
 async def send_messages_with_keyword(app, msg):
@@ -178,7 +98,7 @@ async def send_messages_with_keyword(app, msg):
             'id': document['_id'],
             'file_name': document.get('file_name', 'N/A'),
             'file_caption': document.get('caption', 'N/A'),
-            'file_size': document.get('file_size', '0')
+            'file_size': document.get('file_size', 0)
         } 
         for document in documents
     ]
@@ -209,8 +129,8 @@ async def send_messages_with_keyword(app, msg):
             
             await asyncio.sleep(random.randint(3,6))
         except FloodWait as e:
-            print(f"Sleeping for {e.value} seconds.")
-            await asyncio.sleep(e.value)
+            print(f"Sleeping for {e.x} seconds.")
+            await asyncio.sleep(e.x)
         except Exception as e:
             print(e)
             #await jj.delete()
@@ -218,7 +138,3 @@ async def send_messages_with_keyword(app, msg):
             break
     
     await msg.reply_text("Completed")
-
-
-
-
