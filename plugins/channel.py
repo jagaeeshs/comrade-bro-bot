6d52ff61e41d37547e9dd5796e3cdcdc66760fcc
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-07 20:49:29 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -15,18 +15,21 @@ db = myclient[DATABASE_NAME]
 col = db[COLLECTION_NAME]
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
-async def media(bot, message):
-    """Media Handler"""
-    for file_type in ("document", "video", "audio"):
-        media = getattr(message, file_type, None)
-        if media is not None:
-            break
-    else:
-        return
+async def start(client, message):
+    try:
+        for file_type in ("document", "video", "audio"):
+            media = getattr(message, file_type, None)
+            if media is not None:
+                break
+        else:
+            return
 
-    media.file_type = file_type
-    media.caption = message.caption
-    await save_file(media)
+        media.file_type = file_type
+        media.caption = message.caption
+        await save_file(media)
+        await message.reply_text("Saved In DB")
+    except Exception as e:
+        await message.reply_text(f"Error: {str(e)}")
 
 @Client.on_message(filters.command("savefile") & filters.user(ADMINS))
 async def start(client, message):
