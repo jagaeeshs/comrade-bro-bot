f5b7f66141b52f78727fe9ce60285f77c0cb200f
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-07 08:56:36 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -68,13 +68,16 @@ async def x(app, msg):
     for j, i in enumerate(id_list[last_msg:], start=last_msg):
         try:
             try:
-                await app.send_video(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size']))
+                await app.send_video(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=get_size(int(i['file_size']))))
             except Exception as e:
                 print(e)
-                await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size']))
+                await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=get_size(int(i['file_size']))))
             await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed: {j+1}")
             col.update_one({'_id': 'last_msg'}, {'$set': {'index': j}}, upsert=True)
             await asyncio.sleep(random.randint(1, 3))
+        except FloodWait as e:
+            print(f"Sleeping for {e.x} seconds.")
+            await asyncio.sleep(e.x)
         except Exception as e:
             print(e)
     await jj.delete()
@@ -124,7 +127,7 @@ async def send_messages_with_keyword(app, msg):
                     )
                 )
             
-            await asyncio.sleep(random.randint(4,8))
+            await asyncio.sleep(random.randint(3,6))
         except FloodWait as e:
             print(f"Sleeping for {e.x} seconds.")
             await asyncio.sleep(e.x)
