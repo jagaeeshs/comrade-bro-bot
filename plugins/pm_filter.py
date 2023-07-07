33c66b05c1f2221253367881bfefaa8217c85b79
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-08 04:30:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -38,16 +38,13 @@ req_channel = REQ_CHANNEL
 BUTTONS = {}
 SPELL_CHECK = {}
 
-
 #@Client.on_message(filters.group | filter.private & filters.text & filters.incoming)
 @Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
-       
-	    
-
+   
 @Client.on_callback_query(filters.regex('rename'))
 async def rename(bot,update):
 	user_id = update.message.chat.id
