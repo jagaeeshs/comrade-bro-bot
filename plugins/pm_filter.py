65b35dfa36a792cc138406ce5c508f212f0ffb71
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-08 10:22:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -729,8 +729,10 @@ async def cb_handler(client: Client , query: CallbackQuery):
             elif settings['botpm']:
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
-            else:button = InlineKeyboardButton('🔰 ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ 🔰', url='https://t.me/filmztube'),InlineKeyboardButton('⚜️ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ⚜️', url='https://t.me/+OJPH-0u_62FmZjI9')
-                markup = InlineKeyboardMarkup([[button]])
+            else:
+		button = InlineKeyboardButton('🔰 ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ 🔰', url='https://t.me/filmztube')
+	        button2 = InlineKeyboardButton('⚜️ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ⚜️', url='https://t.me/+OJPH-0u_62FmZjI9')
+                markup = InlineKeyboardMarkup([[button],[button2]])
                 await client.send_cached_media(
                     chat_id=query.from_user.id,
                     file_id=file_id,
@@ -770,8 +772,9 @@ async def cb_handler(client: Client , query: CallbackQuery):
         if f_caption is None:
             f_caption = f"{title}"
         await query.answer()
-        button = InlineKeyboardButton('🔰 ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ 🔰', url='https://t.me/filmztube'),InlineKeyboardButton('⚜️ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ⚜️', url='https://t.me/+OJPH-0u_62FmZjI9')
-        markup = InlineKeyboardMarkup([[button]])
+        button = InlineKeyboardButton('🔰 ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ 🔰', url='https://t.me/filmztube')
+	button2 = InlineKeyboardButton('⚜️ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ⚜️', url='https://t.me/+OJPH-0u_62FmZjI9')
+        markup = InlineKeyboardMarkup([[button],[button2]])
         await client.send_cached_media(
             chat_id=query.from_user.id,
             file_id=file_id,
