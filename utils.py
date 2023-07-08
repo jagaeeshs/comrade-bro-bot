0eb8dcf9bfd9f0815c6b2a31b458bc3d2aed3cc6
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-08 13:23:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ class temp(object):
     U_NAME = None
     B_NAME = None
     SETTINGS = {}
-
+    KEYWORD = {}
 async def is_subscribed(bot, query):
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
