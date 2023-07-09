5d7c6941988654d87b2a5fa370a1e70bf15ff9e3
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-09 16:46:51 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -2,8 +2,8 @@
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
 API_ID = 4234473
-API_HASH = '6d9e82c9edc244b0c0a20d8fa89f9784'
-BOT_TOKEN = '6268719215:AAFXrTuEhXw6j8vP-40yQC35ixD_NnUGqQI'
+API_HASH = ''
+BOT_TOKEN = '626871921540yQC35ixD_NnUGqQI'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -17,8 +17,8 @@ AUTH_USERS = [634637418]
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://karthik:karthik@ac-sry7xij-shard-00-00.wlp6rd4.mongodb.net:27017,ac-sry7xij-shard-00-01.wlp6rd4.mongodb.net:27017,ac-sry7xij-shard-00-02.wlp6rd4.mongodb.net:27017/?ssl=true&replicaSet=atlas-efnevi-shard-0&authSource=admin&retryWrites=true&w=majority"
+DATABASE_URI = "mongodb://karthik::27017,ac-sry7xij-shard-00-01.wlp6rd4.mongodb.net:27017,ac-sry7xij-shard-00-02.wlp6rd4.mongodb.net:27017/?ssl=true&replicaSet=atlas-efnevi-shard-0&authSource=admin&retryWrites=true&w=majority"
 DATABASE_NAME = 'bharath'
-COLLECTION_NAME = 'Anurag_files'  # If you are using the same database, then use different collection name for each bot
+COLLECTION_NAME = ''  # If you are using the same database, then use different collection name for each bot
 
 
