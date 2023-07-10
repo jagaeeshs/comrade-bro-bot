693ad7196028eb770e71c704e579e5926c945752
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-10 14:56:59 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -62,7 +62,7 @@ async def save_file(media):
 
     
 
-    global skip_series
+    skip_series = get_skip_series()
     # Skip saving if the file is part of a series
     if skip_series and is_file_part_of_series(media):
         logger.info(f'Skipping series file: {getattr(media, "file_name", "NO_FILE")}')
