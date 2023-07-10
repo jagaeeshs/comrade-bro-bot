3232927b564c19d82ad7383ad66cbb799ccab94f
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-10 06:38:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -636,14 +636,11 @@ async def skip_series_command(bot, message):
 
 @Client.on_callback_query(filters.regex("^toggle_series$"))
 async def handle_callback(bot, callback_query):
-    '''if callback_query.data == "enable_series":
+    if callback_query.data == "enable_series":
         set_skip_series(True)
     elif callback_query.data == "disable_series":
-        set_skip_series(False)'''
-    if is_skip_series_enabled():
         set_skip_series(False)
-    else:
-        set_skip_series(True)
+    
 
     toggle_text = "Disable Series Skipping" if is_skip_series_enabled() else "Enable Series Skipping"
     callback_data ="toggle_series" # "disable_series" if is_skip_series_enabled() else "enable_series"
