1798d891e8af551963dd1f5f97dd7f1b3e67bec9
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-10 13:14:28 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -8,6 +8,7 @@ from info import *
 import json
 import base64
 from info import ADMINS
+from plugins.channel import skip_series
 from pyrogram.file_id import FileId
 from pyrogram import Client, filters, enums
 from pymongo.errors import DuplicateKeyError
