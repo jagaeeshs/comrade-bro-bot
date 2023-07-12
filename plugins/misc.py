2f98d8abf058e3833a66be9a7caf92699784ba69
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-12 14:39:09 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -162,8 +162,8 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             [
                 InlineKeyboardButton(
                     text='📥 ᴅᴏᴡɴʟᴏᴀᴅ' if download_link else f"{imdb.get('title')}",
-                    #url=download_link[0] if download_link else imdb["url"],
-                    url="https://t.me/WEB_MOVIES_BOT?start=BATCH-BQADBQADEg4AAoBLaVUaQHO5TL4XThYE"
+                    url=download_link[0] if download_link else imdb["url"],
+                    #url="https://t.me/WEB_MOVIES_BOT?start=BATCH-BQADBQADEg4AAoBLaVUaQHO5TL4XThYE"
                 )
             ]
     ]
@@ -200,6 +200,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             url = imdb['url'],
             **locals()
         )
+        
     else:
         caption = "No Results"
     if imdb.get('poster'):
