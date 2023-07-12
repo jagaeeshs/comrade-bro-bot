53dc1aa58f6bf9d4e9812982cf9fba15161fb842
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-12 12:02:06 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -217,19 +217,7 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     await quer_y.answer()
 
 
-'                try:
-                    await message.reply_photo(photo=imdb['poster'], caption=caption, reply_markup=InlineKeyboardMarkup(btn))
-                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-                    pic = imdb.get('poster')
-                    poster = pic.replace('.jpg', "._V1_UX360.jpg")
-                    await message.reply_photo(photo=poster, caption=caption, reply_markup=InlineKeyboardMarkup(btn))
-                except Exception as e:
-                    logger.exception(e)
-                    await message.reply(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
-                await message.delete()
-            else:
-                await message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
-            await query.answer()
+                
 
     
          
