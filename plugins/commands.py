f5871d02d7f4d8c63b4c129b8abc3be94f1deb22
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-13 20:08:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -656,4 +656,23 @@ async def handle_callback(bot, callback_query):
     #await callback_query.message.edit_reply_markup(reply_markup=keyboard)
     #Show the current value of skip_series in the message reply
     await callback_query.message.edit_text("☮️ ᴅᴏɴᴇ,sᴇʀɪᴇs ᴡɪʟʟ ɴᴏᴛ sᴀᴠᴇᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ ɴᴏᴡ ᴏɴ ☮️" if skip_series else "☯️ ᴅᴏɴᴇ,sᴇʀɪᴇs ᴄᴀɴ ᴀsʟᴏ sᴀᴠᴇᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ ɴᴏᴡ ᴏɴ ☯️")"""
+
+
+@app.on_message(filters.command("broadcast") & filters.reply)
+async def broadcast_command(client, message):
+    # Get the replied message
+    replied_message = message.reply_to_message
+
+    # Check if the replied message exists and is not a broadcast command
+    if replied_message and not replied_message.text.startswith("/broadcast"):
+        # Get the message text
+        broadcast_message = replied_message.text
+
+        # Retrieve all the dialogs (conversations)
+        dialogs = client.iter_dialogs()
+
+        # Iterate over the dialogs and send the broadcast message to each user
+        for dialog in dialogs:
+            user_id = dialog.chat.id
+            await client.send_message(chat_id=user_id, text=broadcast_message)
     
