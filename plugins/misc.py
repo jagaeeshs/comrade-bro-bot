e53a6e6710f891de0baafdc7a8713f620d9e217a
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-15 10:02:21 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -169,7 +169,7 @@ async def imdb_search(client, message):
 @Client.on_callback_query(filters.regex('^imdb'))
 async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     
-    i, movie_id, short_link = quer_y.data.split('#')
+    i, movie, short_link = quer_y.data.split('#')
  
     imdb = await get_poster(query=movie, id=True)
     
