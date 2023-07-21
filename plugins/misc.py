c2d10319a9bed43b1854a483f98d9a9fe3745034
Jagadeeshgowdayt <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2023-07-21 20:56:46 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -178,58 +178,32 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     
 
     # Rest of your code...
-
-    btn = [
-        [
-            InlineKeyboardButton(
-                text='📥 Download' if short_link else f"{imdb.get('title')}",
-                url=short_link if short_link else imdb["url"],
-            )
-        ]
-    ]
-    btn = [
+btn = [
     [
         InlineKeyboardButton(
-            text='144P 300 MB' if short_link else f"{imdb.get('title')}",
+            text='📥 Download' if short_link else f"{imdb.get('title')}",
+            url=short_link if short_link else imdb["url"],
+        )
+    ],
+    [
+        InlineKeyboardButton(
+            text="Post to Channel",
+            callback_data=f"imdb_post#{quer_y.message.id}",
+        ),
+        InlineKeyboardButton(
+            text='📥 Download' if short_link else f"{imdb.get('title')}",
+            url=short_link if short_link else imdb["url"],
+        ),
+        InlineKeyboardButton(
+            text='📥 Download' if short_link else f"{imdb.get('title')}",
+            url=short_link if short_link else imdb["url"],
+        ),
+        InlineKeyboardButton(
+            text='📥 Download' if short_link else f"{imdb.get('title')}",
             url=short_link if short_link else imdb["url"],
         ),
     ],
 ]
-
-btn.append([
-    InlineKeyboardButton(
-        text='360P 550 MB' if short_link else f"{imdb.get('title')}",
-        url=short_link if short_link else imdb["url"],
-    ),
-])
-
-btn.append([
-    InlineKeyboardButton(
-        text='720P 900 MB' if short_link else f"{imdb.get('title')}",
-        url=short_link if short_link else imdb["url"],
-    ),
-])
-
-btn.append([
-    InlineKeyboardButton(
-        text='1080P 1.7 GB fHD' if short_link else f"{imdb.get('title')}",
-        url=short_link if short_link else imdb["url"],
-    ),
-])
-
-btn.append([
-    InlineKeyboardButton(
-        text="Post to Channel",
-        callback_data=f"imdb_post#{quer_y.message.id}",
-    ),
-])
-
-btn.append([
-     InlineKeyboardButton(
-            text="Post to Channel",
-            callback_data=f"imdb_post#{quer_y.message.id}",
-        )
-    ])
 
     message = quer_y.message.reply_to_message or quer_y.message
     if imdb:
