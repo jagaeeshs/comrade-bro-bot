a6c54f8def41cbcfdaa9d0711fb0eaa2bd77b921
Jagadeeshgowdayt <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2023-07-22 14:33:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,9 +74,9 @@ LAZY_YT_HANDLE = environ.get('LAZY_YT_HANDLE',None)  # WITHOUT @ [  add only han
 MOVIE_GROUP_USERNAME = environ.get('MOVIE_GROUP_USERNAME', "filmz_tube") #[ without @ ]
 
 # Url Shortner
-URL_MODE = is_enabled((environ.get("URL_MODE","False")), False)
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'urlshortx.com') #Always use website url from api section 
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'eff81ef8a6d0e76498ab428cae7d964731ca6772')
+URL_MODE = is_enabled((environ.get("URL_MODE","True")), True)
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'tnshort.net') #Always use website url from api section 
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'f5760039a7c4bf3fddd00c4297dfad9ff82ce46a')
 LZURL_PRIME_USERS = [int(lazyurlers) if id_pattern.search(lazyurlers) else lazyurlers for lazyurlers in environ.get('LZURL_PRIME_USERS', '634637418').split()]
 
 # Auto Delete For Group Message (Self Delete) #
